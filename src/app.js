e5e7982868156ce09e4e4bb2ff0173ae92eb327e
Tarun Tailor <taruntailor7@gmal.com>
2025-01-09 23:20:03 +0530

    feat: crud api created

--- src/app.js
+++ src/app.js
@@ -4,27 +4,70 @@ const User = require('./models/user');
 
 const app = express();
 
+app.use(express.json());
+
 app.get("/", (req, res) => {
   res.send("Response from Server!");
 });
 
+app.get("/feed", async (req, res) => {
+  try {
+    const users = await User.find({});
+    res.send(users);
+  } catch(err) {
+    res.status(400).send("Something went wrong!");
+  }
+});
+
+app.get("/user", async (req, res) => {
+  try {
+    const user = await User.findOne({email: req.body.email});
+    if(!user) {
+      return res.status(404).send("User not found!");
+    }
+    res.send(user);
+  } catch(err) {
+    res.status(400).send("Something went wrong!");
+  }
+});
+
 app.post("/signup", async (req, res) => {
   // Crearting a new instance of the User model
-  const user = new User({
-    firstName: "Tarun",
-    lastName: "Tailor",
-    email: "tarun@tailor.com",
-    password: "" // Random
-  });
+  const user = new User(req.body);
 
   try {
     await user.save();
     res.send("User Added successfully!");
   } catch(err) {
-    res.status(400).send("Error saving the user:" + err.message);
+    res.status(400).send("Something went wrong!");
   }
   
 });
+
+app.delete("/user", async (req, res) => {
+  const userId = req.body.userId;
+  try {
+    const user = await User.findByIdAndDelete(userId);
+    res.send("User deleted sucessfully!");
+  } catch (error) {
+    res.status(400).send("Something went wrong!");
+  }
+})
+
+app.patch("/user", async (req, res) => {
+  const userId = req.body.userId;
+  const data = req.body;
+  console.log('userId', userId);
+  console.log('data', data);
+  try {
+    const user = await User.findByIdAndUpdate({_id: userId}, data, {
+      returnDocument: "after" // by default before is value
+     });
+    res.send("User updated sucessfully!");
+  } catch (error) {
+    res.status(400).send("Something went wrong!");
+  }
+})
 
 const port = process.env.PORT || 5000;
 
