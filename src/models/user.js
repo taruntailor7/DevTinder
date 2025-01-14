de80ceb57136f2521ac5246f49d9b2ec4cec9bcf
Tarun Tailor <taruntailor7@gmail.com>
2025-01-14 22:23:28 +0530

    feat: interested and ignored request api creadted with all validations and also

--- src/models/user.js
+++ src/models/user.js
@@ -41,11 +41,15 @@ const userSchema = new mongoose.Schema({
     },
     gender: {
         type: String,
-        validate(value){
-            if(!['male', 'female', 'others'].includes(value)){
-                throw new Error("Not a valid gender");
-            }
+        enum: {
+            values: ['male', 'female', 'others'],
+            message: `{VALUE} is not a valid gender type`,
         }
+        // validate(value){
+        //     if(!['male', 'female', 'others'].includes(value)){
+        //         throw new Error("Not a valid gender");
+        //     }
+        // }
     },
     photoUrl: {
         type: String,
--- src/routes/request.js
+++ src/routes/request.js
@@ -1,15 +1,72 @@
 const express = require("express");
 const { userAuth } = require("../middlewares/auth");
+const ConnectionRequest = require("../models/connectionRequest");
+const User = require("../models/user");
+const mongoose = require("mongoose");
 
 const requestRouter = express.Router();
 
-requestRouter.post("/sendConnectionRequest", userAuth, async (req, res) => {
+requestRouter.post("/send/:status/:toUserId", userAuth, async (req, res) => {
   try {
-    const user = req.user;
+    const fromUserId = req.user._id;
+    const toUserId = req.params.toUserId;
+    const status = req.params.status;
 
-    res.send(user.firstName + " Send Request");
+    // This we can do in modal also
+    // if(fromUserId.equals(toUserId)) {
+    //   return res.status(400).json({
+    //     message: "User can not send request to him/herself"
+    //   });
+    // }
+
+    if (!mongoose.Types.ObjectId.isValid(toUserId)) {
+      return res.status(400).json({
+        message: "Invalid User ID format!",
+      });
+    }
+
+    const allowedStatus = ['interested', 'ignored'];
+
+    if(!allowedStatus.includes(status)) {
+      return res.status(400).json({
+        message:  "Invalid Status type: " + status,
+      })
+    }
+
+    const toUser = await User.findById(toUserId);
+    if(!toUser) {
+      return res.status(404).json({
+        message: 'User not found!',
+      });
+    }
+
+    const existingConnectionRequest = await ConnectionRequest.findOne({
+      $or: [
+        { fromUserId, toUserId },
+        { fromUserId: toUserId, toUserId: fromUserId },
+      ],
+    });
+
+    if(existingConnectionRequest) {
+      return res.status(400).json({
+        message: "Connection Request Already Exists!"
+      });
+    }
+
+    const connectionRequest = new ConnectionRequest({
+      fromUserId, toUserId, status
+    })
+
+    const data = await connectionRequest.save();
+
+    res.json({
+      message: "Interested/Ignored Request Sent Successfully!",
+      data,
+    });
   } catch (err) {
-    res.status(400).send("ERROR while signup: " + err.message);
+    res.status(400).json({
+      message: err.message
+    });
   }
 });
 
