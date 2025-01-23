66d535dd19c345d37267315984794fc52d9e35ab
Tarun Tailor <taruntailor7@gmail.com>
2025-01-23 23:05:43 +0530

    feat: events added for sendMessage and received message and room ids created for caht between 2 users

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -1,4 +1,9 @@
 const socket = require("socket.io");
+const crypto = require('crypto');
+
+const getSecretRoomId = (userId, targetUserId) => {
+    return crypto.createHash("sha256").update([userId, targetUserId].sort().join("_")).digest("hex");
+}
 
 const initializeSocket = (server) => {
   const io = socket(server, {
@@ -10,12 +15,20 @@ const initializeSocket = (server) => {
   io.on("connection", (socket) => {
     // Handle events
 
-    socket.on('joinChat', () => {
+    socket.on('joinChat', ({firstname, userId, targetUserId}) => {
+        const roomId =  getSecretRoomId(userId, targetUserId); // sorting for making roomId same for 2 users
 
+        console.log(firstname + " joined room: " + roomId);
+
+        socket.join(roomId);
     });
 
-    socket.on('sendMessage', () => {
+    socket.on('sendMessage', ({ firstname, userId, targetUserId, text }) => {
+        const roomId =  getSecretRoomId(userId, targetUserId);
 
+        console.log(firstname + " : " + text);
+
+        io.to(roomId).emit('messageReceived', {firstname, text})
     });
 
     socket.on('disconnect', () => {
